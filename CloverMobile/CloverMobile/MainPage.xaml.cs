7b04573217aff9eb22540d3fcaa34962b2167486
Thomster <none@none>
2011-04-09 20:09:51 +0300

    MobileApp: Authenticating & setting file saving to memory card in progress

--- CloverMobile/CloverMobile/MainPage.xaml.cs
+++ CloverMobile/CloverMobile/MainPage.xaml.cs
@@ -15,18 +15,50 @@ using System.Windows.Threading;
 
 using System.IO;
 using CloverMobile;
+using System.IO.IsolatedStorage;
 
 namespace CloverMobile
 {
     public partial class MainPage : PhoneApplicationPage
     {
-        Controller controller; 
-        
+        Controller controller;
+        string fileName = "settings.txt";
         // ** Constructor
         public MainPage()
         {
             InitializeComponent();
             controller = Controller.getInstance;
+            // ** Check the setting here, if the setting file exists, don't display the splashscreen, otherwise, display it
+            var appStorage = IsolatedStorageFile.GetUserStoreForApplication();
+            if (appStorage.FileExists(fileName) == false)
+            {
+                splashScreen.Visibility = System.Windows.Visibility.Collapsed;
+                //logIn("testipaavo", "testi2");
+                // ** dont display the splashscreen
+            }
+            else
+            {
+                PageTitle.Text = "Log In";
+                splashScreen.Visibility = System.Windows.Visibility.Visible;
+                // ** authenticate first, if successfull, then create the file
+                if (controller.authenticate(userNameTextBox.Text, passwordTextBox.Text) == true)
+                {                    
+                    if (appStorage.FileExists("settings.txt") == false)
+                    { 
+
+                        // create a new file
+                        using (var file = appStorage.OpenFile(fileName, System.IO.FileMode.Create, System.IO.FileAccess.Write))
+                        {   
+                            using (var writer = new StreamWriter(file))
+                            {
+                                writer.Write(userNameTextBox.Text + " " + passwordTextBox.Text);
+                            }
+                        }
+                    }     
+                }    
+            }
+            // splashScreen.visibility = System.Windows.Visibility.Visible/Collabsed
+            // get the username and password from memory card          
             rotateClover.Begin();
             rotateClover.Pause();
 
@@ -54,6 +86,20 @@ namespace CloverMobile
         private void windImage_ImageFailed(object sender, ExceptionRoutedEventArgs e)
         {
 
+        }
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            // check the 
+        }
+
+        private void passwordTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // delete this
+        }
+
+        private void userNameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // delete this
         }
     }
 }
